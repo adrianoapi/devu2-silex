a9183e528af233fcd2d5e28acf3b4668480a2753
Adriano A Costa <sdcomputadores@gmail.com>
2018-01-15 17:03:45 -0200

    add route post

--- public/index.php
+++ public/index.php
@@ -2,10 +2,23 @@
 
 require '../vendor/autoload.php';
 $app = new Silex\Application();
+
+/*
+ * Rotas
+ */
 $app->get('/', function() {
-    return 'home';
+    return '<form method="POST" action="/home">' .
+            '   <input name="nome">' .
+            '   <button type="submit">Enviar</button>' .
+            '</form>';
 });
+
+$app->post('/home', function() {
+    return 'Post enviado com sucesso!';
+});
+
 $app->get('/hello-world', function() {
     return 'Hello world';
 });
+
 $app->run();
