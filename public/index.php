5b8dde9934c72e42e366550f65270a53e955580a
Adriano A Costa <sdcomputadores@gmail.com>
2018-01-15 17:38:46 -0200

    request com HttpFoundation

--- public/index.php
+++ public/index.php
@@ -1,24 +1,38 @@
 <?php
 
-require '../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
+
 
 /*
  * Rotas
  */
-$app->get('/', function() {
-    return '<form method="POST" action="/home">' .
-            '   <input name="nome">' .
-            '   <button type="submit">Enviar</button>' .
-            '</form>';
-});
-
-$app->post('/home', function() {
-    return 'Post enviado com sucesso!';
-});
 
 $app->get('/hello-world', function() {
     return 'Hello world';
 });
+
+$app->get('/home', function() {
+    return NULL .
+            '<form method="get" action="/get-name">' .
+            '   <input type="text" name="nome" value="">' .
+            '   <button type="submit">Enviar</button>' .
+            '</form>';
+});
+
+// Request com GET
+$app->get('/get-name', function(Request $request) {
+    $name = $request->get('nome','sem nome');
+    return "Post enviado com sucesso! <br> $name";
+});
+
+// Request com POST
+$app->post('/home', function(Request $request) {
+    $name = $request->get('nome','sem nome');
+    return "Post enviado com sucesso! <br> $name";
+});
+
 
 $app->run();
